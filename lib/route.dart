5b8d36d78b6bda4ec6470ab67d84f66c83d7041a
林创湖 <linchuanghu@addcn.com>
2023-07-28 17:14:22 +0800

    移除V1的相关文件

--- lib/route.dart
+++ lib/route.dart
@@ -1,5 +1,2 @@
 library ad_route;
 
-export 'route.dart';
-export 'route/animation.dart';
-export 'route/route_intercept.dart';
--- test/route_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:route/route.dart';
-
-void main() {
-  test('adds one to input values', () {
-
-  });
-}
