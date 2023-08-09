8231e6188c50660e8c4dca568fd6ba9ba8402757
林创湖 <linchuanghu@addcn.com>
2023-08-09 08:59:34 +0800

    fixed: snackbar使用的overlayContext未初始化

--- lib/route/go_navigator.dart
+++ lib/route/go_navigator.dart
@@ -4,11 +4,11 @@ import 'package:get/get_core/get_core.dart';
 import 'package:get/get_navigation/src/bottomsheet/bottomsheet.dart';
 import 'package:get/get_navigation/src/dialog/dialog_route.dart';
 import 'package:get/get_navigation/src/snackbar/snackbar.dart';
-import 'package:get/get_navigation/src/snackbar/snackbar_controller.dart';
 import 'package:get/get_utils/src/platform/platform.dart';
 import 'package:go_router/go_router.dart';
 
 import 'go_navigator_observer.dart';
+import 'snackbar_controller.dart';
 
 part 'go_navigator_extension.dart';
 
