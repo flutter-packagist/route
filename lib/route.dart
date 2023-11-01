1be8325668ac9f4beb897a6591a25d0cfd9150d3
LioLin <windows7lake@gmail.com>
2023-11-01 16:26:03 +0800

    feat: export文件更新

--- lib/route.dart
+++ lib/route.dart
@@ -1,5 +1,8 @@
 library ad_route;
 
+export 'package:go_router/go_router.dart';
+
 export 'route/go_navigator.dart';
 export 'route/go_navigator_observer.dart';
 export 'route/route_extension.dart';
+export 'route/route_transition.dart';
