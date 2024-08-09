c741af0856fca96eee9d66d58e3d23399cf8d261
LioLin <windows7lake@gmail.com>
2024-08-09 14:56:18 +0800

    fixed: bottomSheet空指针问题

--- lib/route/go_navigator.dart
+++ lib/route/go_navigator.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:get/get_core/get_core.dart';
-import 'package:get/get_navigation/src/bottomsheet/bottomsheet.dart';
 import 'package:get/get_navigation/src/dialog/dialog_route.dart';
 import 'package:get/get_navigation/src/snackbar/snackbar.dart';
 import 'package:get/get_utils/src/platform/platform.dart';
 import 'package:go_router/go_router.dart';
 
+import 'bottomsheet.dart';
 import 'go_navigator_observer.dart';
 import 'snackbar_controller.dart';
 
